ecfe3b7996ff2053840d35eec5720b6a0476de57
ramune64 <ramune69atrrsc@gmail.com>
2024-06-19 20:27:25 +0900

    フロントページのファイル作成

--- index.php
+++ index.php
@@ -1,144 +1,31 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta charset="UTF-8">
-  <title>練習中1</title>
-  <?php include "links.html" ?>
+  <title>練習中2</title>
   <!-- <link rel="stylesheet" href="aaa.css" type="text/css">
   <link rel="icon" href="image\favicon.ico"> -->
+  <?php include "links.html" ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8">
 </head>
 <body>
-    <?php include "header-nav.php" ?>
-    <div class="wrap">
-    <div id="hontai" class="hontai">
+        <?php include "header-nav.php" ?>
+        <div class="wrap">
+        <div id="hontai">
         <div class="main_content">
-        <div class="inner_content" id="page0-1">
-        <h1>一蘭</h1>
-        <div class="all_view">
-            <div class="view">
-                <h3>index0</h3>
-                <p>あああああああうあああああああええええええええええええええええええええええココああああああああああああああああああああああああああああああああああああああ野球あおおおおおおおおお明日おおおお昨日おおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおあ</p>
+            <div class="inner_content" id="page1-2">
+                
             </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああ先週ああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああ今週うううううううううううう</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ううううううううううううううううううううううううううううううううううううううううううううううううううううううううう</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああごめんああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああend</p>
+            <div class="inner_content" id="page1-3">
+                
             </div>
         </div>
-        </div>
-    <div class="inner_content" id="page0-2">
-        <h1>index1</h1>
-        <div class="all_view">
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-        </div>
-    </div>
-    <div class="inner_content" id="page0-3">
-        <h1>index2</h1>
-        <div class="all_view">
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-        </div>
-    </div>
-    <div class="inner_content" id="page0-4">
-        <h1>index3</h1>
-        <div class="all_view">
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-            <div class="view">
-                <h3>○○</h3>
-                <p>ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</p>
-            </div>
-        </div>
-    </div>
-    </div>
-    <div class="side_content">
+        <div class="side_content">
     <?php include "Side_bar.html" ?>
     </div>
     </div>
     <?php include "footer.html" ?>
-    </div>
+        </div>
+    
 </body>
 </html>
