fad034498efcd11f53d4154562e5364cd20f9cb4
tqck <harata9mi0902@gmail.com>
2024-07-18 17:03:00 +0900

    konnnitiha

--- intro_part.php
+++ intro_part.php
@@ -15,13 +15,15 @@
         <div class="main_content">
             	<div class="inner_content inner_top intro_part" id="intro_part">
                     <h1 class="part_h1">パート紹介</h1>
-                    <div class="part">
-                        <h2><!-- <a href="#" class="batsu"></a> -->本部パート</h2>
+                    <div class="part" style="background-color: black;">
+                        <h2><!-- <a href="#" class="batsu"></a> -->本部パート</h2>        
                         <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change">"こう"</span>(style.cssの最後の方を見てね。)</p>
+                        <p class="Pl_name">PL.洛星太郎</p>
                     </div>
-                    <div class="part">
+                    <div class="part" style="background-color: gray;">
                         <h2>執行パート</h2>
                         <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change2">"こう"</span>(style.cssの最後の方を見てね。)</p>
+                        <p class="Pl_name">PL.洛星太郎</p>
                     </div>
                     <div class="part">
                         <h2>大講堂パート</h2>
@@ -31,17 +33,20 @@
                         <h2>効果パート</h2>
                         <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b>部分的に色を変えたかったら<span class="iro_change4">"こう"</span>(style.cssの最後の方を見てね。)</p>
                     </div>
-                    <div class="part">
-                        <h2>演劇パート</h2>
-                        <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b>部分的に色を変えたかったら<span class="uwaaaaa">"こう"</span>(style.cssの最後の方を見てね。)</p>
+                    <div class="part" style="background-color: green;">
+                        <h2>会計パート</h2>
+                        <p>元弓でのノコギリやドライバーなどの道具やベニヤ板や木材の貸出をしています。<br>PLは記念祭会計の仕事もしています。会計は文化祭の要！！</p>
+                        <p class="Pl_name">PL.井上 友仁</p>
                     </div>
-                    <div class="part">
-                        <h2>小劇場パート</h2>
-                        <p>小劇場で漫才、大喜利、弾き語りをお楽しみください。</p>
+                    <div class="part" style="background-color: purple;">
+                        <h2>式典パート</h2>
+                        <p>文化祭の開閉会式を司るパートです。開会式で10分弱の劇をし、生徒全体の雰囲気を盛り上げ文化祭ムードに変えるのが最も重要な役割です。開会式を一般の方に公開できないのは悲しいですが、精一杯文化祭を盛り上げます！皆さん楽しんでください！</p>
+                        <p class="Pl_name">PL.黒田 陽太郎</p>
                     </div>
-                    <div class="part">
+                    <div class="part" style="background-color: #30D5C8;">
                         <h2>合唱パート</h2>
-                        <p>合唱パートは合唱コンクールの運営をするパートです。みんなが気持ちよく歌えるように全力でサポートします。</p>
+                        <p>毎年文化祭の初日に行われる合唱コンテスト。私たち合唱パートはその大事な初日を任されている。全校が参加するため非常に大規模であるが、各自連携をとりつつ円滑な運営を進めている。また音楽の才能を生かして伴奏の譜めくりや合唱の審査を行う者もいる。</p>
+                        <p class="Pl_name">PL.甲斐 悠真</p>
                     </div>
                     <div class="part">
                         <h2>展示パート</h2>
@@ -80,8 +85,8 @@
                         <p>ｘｘで生徒が製作した番組を放送しています。さらに校内で定刻放送をします</p>
                     </div>
                     <div class="part">
-                        <h2>式典パート</h2>
-                        <p>文化祭の最初と最後を飾る、開会式と閉会式を最高の場にすることに全力を注ぐパートです。</p>
+                        <h2>小劇場パート</h2>
+                        <p>小劇場で漫才、大喜利、弾き語りをお楽しみください。</p>
                     </div>
 				</div>
 			</div>
