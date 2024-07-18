621f20a93b1078d86cf469051196519762a442c0
ramune64 <ramune69atrrsc@gmail.com>
2024-07-18 23:56:42 +0900

    aaaaaaaaaaaaa

--- intro_part.php
+++ intro_part.php
@@ -13,81 +13,55 @@
     <div class="wrap">
         <div id="hontai">
         <div class="main_content">
-            	<div class="inner_content inner_top intro_part" id="intro_part">
-                    <h1 class="part_h1">パート紹介</h1>
-                    <div class="part" style="background-color: black;">
-                        <h2><!-- <a href="#" class="batsu"></a> -->本部パート</h2>        
-                        <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change">"こう"</span>(style.cssの最後の方を見てね。)</p>
-                        <p class="Pl_name">PL.洛星太郎</p>
-                    </div>
-                    <div class="part" style="background-color: gray;">
-                        <h2>執行パート</h2>
-                        <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change2">"こう"</span>(style.cssの最後の方を見てね。)</p>
-                        <p class="Pl_name">PL.洛星太郎</p>
-                    </div>
-                    <div class="part">
-                        <h2>大講堂パート</h2>
-                        <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b>部分的に色を変えたかったら<span class="iro_change3">"こう"</span>(style.cssの最後の方を見てね。)</p>
-                    </div>
-                    <div class="part">
-                        <h2>効果パート</h2>
-                        <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b>部分的に色を変えたかったら<span class="iro_change4">"こう"</span>(style.cssの最後の方を見てね。)</p>
-                    </div>
-                    <div class="part" style="background-color: green;">
-                        <h2>会計パート</h2>
-                        <p>元弓でのノコギリやドライバーなどの道具やベニヤ板や木材の貸出をしています。<br>PLは記念祭会計の仕事もしています。会計は文化祭の要！！</p>
-                        <p class="Pl_name">PL.井上 友仁</p>
-                    </div>
-                    <div class="part" style="background-color: purple;">
-                        <h2>式典パート</h2>
-                        <p>文化祭の開閉会式を司るパートです。開会式で10分弱の劇をし、生徒全体の雰囲気を盛り上げ文化祭ムードに変えるのが最も重要な役割です。開会式を一般の方に公開できないのは悲しいですが、精一杯文化祭を盛り上げます！皆さん楽しんでください！</p>
-                        <p class="Pl_name">PL.黒田 陽太郎</p>
-                    </div>
-                    <div class="part" style="background-color: #30D5C8;">
-                        <h2>合唱パート</h2>
-                        <p>毎年文化祭の初日に行われる合唱コンテスト。私たち合唱パートはその大事な初日を任されている。全校が参加するため非常に大規模であるが、各自連携をとりつつ円滑な運営を進めている。また音楽の才能を生かして伴奏の譜めくりや合唱の審査を行う者もいる。</p>
-                        <p class="Pl_name">PL.甲斐 悠真</p>
-                    </div>
-                    <div class="part">
-                        <h2>展示パート</h2>
-                        <p>各展示団体と協力してより良い展示を作り上げます。</p>
-                    </div>
-                    <div class="part">
-                        <h2>バザーパート</h2>
-                        <p>生徒バザーの準備・開催をしているパートです。当日はｘｘで行っているのでぜひ来てください。</p>
-                    </div>
-                    <div class="part">
-                        <h2>模擬店パート</h2>
-                        <p>文化祭2日目、3日目にピロティーで模擬店やっています。今年から調理を復活したので楽しんでもらえると思えます。ぜひ来てください。</p>
-                    </div>
-                    <div class="part">
-                        <h2>ライブパート</h2>
-                        <p>文化祭といえばライブでしょ！各ライブ団体の管理及び指揮を行っています。ぜひ足を運んでください。</p>
-                    </div>
-                    <div class="part">
-                        <h2>アミューズパート</h2>
-                        <p>４階で小さいお子様から大人の方まで楽しめるアミューズメントを行っています。</p>
-                    </div>
-                    <div class="part">
-                        <h2>プログラムパート</h2>
-                        <p>今あなたの持っている素晴らしいパンフレットを作成するパートです。<br>パンフレットのデータは<a href="download.php">こちら</a>からもご確認できます。</p>
-                    </div>
-                    <div class="part">
-                        <h2>装飾パート</h2>
-                        <p>校内の階段・天井・壁などの装飾を担当しています。絵のうまい下手は気にせず、温かい目で見てくれればと思います。</p>
-                    </div>
-                    <div class="part">
-                        <h2>会計パート</h2>
-                        <p>元弓と呼ばれる大講堂下倉庫で道具の貸し出しを行っています。また、予算の申請と決算を行っています。</p>
-                    </div>
-                    <div class="part">
-                        <h2>ラジオパート</h2>
-                        <p>ｘｘで生徒が製作した番組を放送しています。さらに校内で定刻放送をします</p>
-                    </div>
-                    <div class="part">
-                        <h2>小劇場パート</h2>
-                        <p>小劇場で漫才、大喜利、弾き語りをお楽しみください。</p>
-                    </div>
+                <div class="inner_content inner_top intro_part" id="intro_part">
+                    <h1>パート紹介</h1>
+                        <div class="Flex Flex_wrap">
+                            <div class="part">
+                                <h2><!-- <a href="#" class="batsu"></a> -->本部パート</h2>
+                                <div class="Flex">
+                                    <img src="image/t-shirt.png" alt="">
+                                    <img src="image/t-shirt.png" alt="">
+                                </div>        
+                                <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change">"こう"</span>(style.cssの最後の方を見てね。)</p>
+                                <p class="Pl_name">PL.洛星太郎</p>
+                            </div>
+                            <div class="part">
+                                <h2>本部パート</h2>
+                                <div class="Flex">
+                                    <img src="image/t-shirt.png" alt="">
+                                    <img src="image/t-shirt.png" alt="">
+                                </div>        
+                                <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change">"こう"</span>(style.cssの最後の方を見てね。)</p>
+                                <p class="Pl_name">PL.洛星太郎</p>
+                            </div>
+                            <div class="part">
+                                <h2>本部パート</h2>
+                                <div class="Flex">
+                                    <img src="image/t-shirt.png" alt="">
+                                    <img src="image/t-shirt.png" alt="">
+                                </div>        
+                                <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change">"こう"</span>(style.cssの最後の方を見てね。)</p>
+                                <p class="Pl_name">PL.洛星太郎</p>
+                            </div>
+                            <div class="part">
+                                <h2>本部パート</h2>
+                                <div class="Flex">
+                                    <img src="image/t-shirt.png" alt="">
+                                    <img src="image/t-shirt.png" alt="">
+                                </div>        
+                                <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change">"こう"</span>(style.cssの最後の方を見てね。)</p>
+                                <p class="Pl_name">PL.洛星太郎</p>
+                            </div>
+                            <div class="part">
+                                <h2>本部パート</h2>
+                                <div class="Flex">
+                                    <img src="image/t-shirt.png" alt="">
+                                    <img src="image/t-shirt.png" alt="">
+                                </div>        
+                                <p>ここはこんなパートです。<br>←改行はこうやってやるよ。<b>太字はこう。</b><u>下線は多分こう。</u>部分的に色を変えたかったら<span class="iro_change">"こう"</span>(style.cssの最後の方を見てね。)</p>
+                                <p class="Pl_name">PL.洛星太郎</p>
+                            </div>
+                        </div>
 				</div>
 			</div>
         <div class="side_content">
