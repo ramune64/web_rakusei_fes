549a400d52206f79081a8382c9d67b582acc5300
ramune64 <ramune69atrrsc@gmail.com>
2024-08-19 16:27:35 +0900

    wa-i

---
+++ live_intro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title></title>
+  <?php include "links.html" ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8">
+</head>
+<body>
+        <?php include "header-nav.php" ?>
+        <div class="wrap">
+        <div id="hontai">
+        <div class="main_content">
+            <div class="inner_content inner_top" id="page1-2">
+                
+            </div>
+            <div class="inner_content" id="page1-3">
+                
+            </div>
+        </div>
+    </div>
+    </div>
+    <?php include "footer.html" ?>
+        </div>
+    
+</body>
+</html>
