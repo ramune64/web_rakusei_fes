2b83dec6217725941bf39dd007fbbc46f137dee2
tqck <harata9mi0902@gmail.com>
2024-08-20 17:22:13 +0900

    aaaaa

--- time_table.php
+++ time_table.php
@@ -17,15 +17,15 @@
             <h1>タイムテーブル</h1>
             <div class="buttons">
                 <ul class="buttons_top">
-                    <a href="javascript:tt_day(1)" class="button_ts b_days" id="d1b"><li class="button_li">ここクリック1</li></a>
-                    <a href="javascript:tt_day(2)" class="button_ts b_days" id="d2b"><li class="button_li">ここクリック2</li></a>
-                    <a href="javascript:tt_day(3)" class="button_ts b_days" id="d3b"><li class="button_li">ここクリック3</li></a>
+                    <a href="javascript:tt_day(1)" class="button_ts b_days" id="d1b"><li class="button_li">壱日目   .13日</li></a>
+                    <a href="javascript:tt_day(2)" class="button_ts b_days" id="d2b"><li class="button_li">弐日目   .14日</li></a>
+                    <a href="javascript:tt_day(3)" class="button_ts b_days" id="d3b"><li class="button_li">参日目   .15日</li></a>
                 </ul>
                 <ul class="buttons_buttom place_button" id="d1">
-                    <a href="javascript:tt_place(1)" class="button_ts" id="d1p1b"><li class="button_li">ここクリック1</li></a>
-                    <a href="javascript:tt_place(2)" class="button_ts" id="d1p2b"><li class="button_li">ここクリック2</li></a>
-                    <a href="javascript:tt_place(3)" class="button_ts" id="d1p3b"><li class="button_li">ここクリック3</li></a>
-                    <a href="javascript:tt_place(4)" class="button_ts" id="d1p4b"><li class="button_li">ここクリック4</li></a>
+                    <a href="javascript:tt_place(1)" class="button_ts" id="d1p1b"><li class="button_li">大講堂</li></a>
+                    <a href="javascript:tt_place(2)" class="button_ts" id="d1p2b"><li class="button_li">ケルブ講堂</li></a>
+                    <a href="javascript:tt_place(3)" class="button_ts" id="d1p3b"><li class="button_li">大教室</li></a>
+                    <a href="javascript:tt_place(4)" class="button_ts" id="d1p4b"><li class="button_li">家庭科室</li></a>
                 </ul>
                 <ul class="buttons_buttom place_button" id="d2">
                     <a href="javascript:tt_place(1)" class="button_ts" id="d2p1b"><li class="button_li">ここクリック21</li></a>
