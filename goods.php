c5c5e6cb743bbcd8ee2715bc40283251bcd81cfb
tqck <harata9mi0902@gmail.com>
2024-08-28 05:58:21 +0900

    dakyou

--- goods.php
+++ goods.php
@@ -19,17 +19,61 @@
                 <section>
                     <h2><u>寄贈品</u></h2>
                     <div class="goods_img">
-                        <img src="image/slide_img.png" alt="">
-                        <img src="image/slide_img.png" alt="">
+                        <img src="image/goods-1.png" alt="">
                     </div>
-                    <p>こんなものやあんなものがたくさん！！（仮）こんなものやあんなものがたくさん！！（仮）こんなものやあんなものがたくさん！！（仮）こんなものやあんなものがたくさん！！（仮）こんなものやあんなものがたくさん！！（仮）</p>
+                    <p>生徒の皆さん、教職員の皆さんが寄贈してくださった文房具、本、アクセサリー、小物などを販売しています。生徒だけでなく様々なニーズに沿った品揃えとなっております。掘り出し物があるかも…！</p>
                 </section>
                 <section>
-                    <h2><u>寄贈品2</u></h2>
+                    <h2><u>マフラータオル</u></h2>
                     <div class="goods_img">
-                        <img src="image/slide_img.png" alt="">
+                        <img src="image/simple towel.jpg"  alt="">
                     </div>
-                    <p>こんなものやあんなものがたくさん！！（仮）こんなものやあんなものがたくさん！！（仮）こんなものやあんなものがたくさん！！（仮）こんなものやあんなものがたくさん！！（仮）こんなものやあんなものがたくさん！！（仮）</p>
+                    <p>触り心地の良いコットンシャーリングマフラータオルです。<br>1枚800円</p>
+                </section>
+                <section>
+                    <h2><u>ハンドタオル（ハンカチ）</u></h2>
+                    <div class="goods_img">
+                        <img src="image/handtowel.jpg" alt="">
+                    </div>
+                    <p>ハンカチサイズの普段使いにもってこいなタオルです。<br>1枚650円</p>
+                </section>
+                <section>
+                    <h2><u>シャープペンシル</u></h2>
+                    <div class="goods_img">
+                        <img src="image/smash.jpg" alt="">
+                    </div>
+                    <p>毎年大人気な書きやすさを重視したシャーペンのsmash！今年も、ブラック、レッド、ダークグレーの３色ご用意しております第73回洛星文化祭の印字が入った今年しか買えない品物です。<br>一本1000円</p>
+                </section>
+                <section>
+                    <h2><u>シャープペンシル(ピュアモルト)</u></h2>
+                    <div class="goods_img">
+                        <img src="image/woodpen.jpg" alt="">
+                    </div>
+                    <p>昨年すぐに完売してしまった木軸シャーペンを今年は二色！！（ナチュラル,ダークブラウン）<br>を数量限定でご用意しました！第73回洛星文化祭の印字が入った今年しか買えない品物です。<br>一本1100円</p>
+                </section>
+                <section>
+                    <h2><u>ボールペン(ジェットストリーム4色+1)</u></h2>
+                    <div class="goods_img">
+                        <img src="image/jetstream.jpg" alt="">
+                    </div>
+                    <p>言わずと知れた使いやすさ抜群でなおかつ、シャーペンの機能もついて一石二鳥なボールペン。ブラック、ネイビー、ボルドー、パープル、ライトピンクの５色ご用意しております。<br>一本1000円</p>
+                </section>
+                <section>
+                    <h2><u>マグカップ</u></h2>
+                    <div class="goods_img">
+                    </div>
+                    <p>今年のロゴの入ったマグカップですデザインはお楽しみに！！<br>1個xx円</p>
+                </section>
+                <section>
+                    <h2><u>缶バッジ</u></h2>
+                    <div class="goods_img">
+                        <img src="image/bazzi.jpg" alt="">
+                    </div>
+                    <p>今年のロゴのキーホルダーを毎日数量限定で販売します<br>1個　350円</p>
+                </section>
+                <section>
+                    <h2><u>洛星合格祈願鉛筆</u></h2>
+                    <p>全ての受験生を応援します！がんばれ受験生！！<br>3本入り300円</p>
                 </section>
             </div>
         </div>
