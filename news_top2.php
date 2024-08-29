398d8d4b986cc2a5e51cfa29fff7387d732644b4
ramune64 <ramune69atrrsc@gmail.com>
2024-08-30 03:24:53 +0900

    news

---
+++ news_top2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="UTF-8">
+  <title>第73回洛星文化祭～宴～</title>
+  <!-- <link rel="stylesheet" href="aaa.css" type="text/css">
+  <link rel="icon" href="image\favicon.ico"> -->
+  <?php include "links.html" ?>
+</head>
+<body>
+    <?php include "header-nav.php" ?>
+    <div class="wrap">
+    <div id="hontai">
+    <div class="main_content">
+    <div class="inner_content inner_top" id="page3-1">
+        <h1>お知らせ</h1>
+            <div class="news_topics">
+                <a href="0news.php">
+                    <h3>2024/08/28洛星文化祭ホームページ公開！</h3>
+                    <p>
+                        文化祭ホームページを公開しました!校内マップ、タイム...
+                    </p>
+                </a>
+            </div>
+    
+    </div>
+</div>
+<div class="side_content">
+    <?php include "Side_bar.html" ?>
+    </div>
+    </div>
+    <?php include "footer.html" ?>
+</div>
+    
+</body>
+</html>
