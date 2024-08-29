1629f367fc723986cc063480ab1cd6793843ab54
tqck <harata9mi0902@gmail.com>
2024-08-29 11:12:30 +0900

    ？？？

--- 0news.php
+++ 0news.php
@@ -9,16 +9,15 @@
 <body>
 <?php include "header-nav.php" ?>
   <div class="wrap">
-     <div id="hontai">
-        <div class="main_content">
-           <div class="inner_content inner_top news" id="news0">
-             <h1>2024/08/29<br>洛星文化祭ホームページ公開！</h1>
-              <p>文化祭ホームページを公開しました!
-                <br>校内マップ、タイムテーブル、団体の一覧など、色々な情報が確認できます。<br>また、お知らせや新たな情報が出た場合随時更新する予定です。
-              </p>
-           </div>
-        </div>
-     </div>
+       <div id="hontai">
+          <div class="main_content">
+             <div class="inner_content inner_top news" id="news0">
+                  <h1>2024/08/29<br>洛星文化祭ホームページ公開！</h1>
+                  <p>文化祭ホームページを公開しました!
+                  <br>校内マップ、タイムテーブル、団体の一覧など、色々な情報が確認できます。<br>また、お知らせや新たな情報が出た場合随時更新する予定です。</p>
+             </div>
+          </div>
+       </div>
      <?php include "footer.html" ?>
  </div>
 </body>
