26351afb17114773270fd7c22e52dda99899d3f4
tqck <harata9mi0902@gmail.com>
2024-09-07 19:38:57 +0900

    aaa

--- index_novideo.php
+++ index_novideo.php
@@ -3,11 +3,24 @@
 <head>
   <title>第73回洛星文化祭～宴～</title>
   <?php include "links.html" ?>
+
+ <!-- Google Tag Manager -->
+ <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+ 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+ })(window,document,'script','dataLayer','GTM-T5TBPZR5');</script>
+ <!-- End Google Tag Manager -->
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="../stylesheet/front.css" type="text/css">
   <meta charset="UTF-8">
 </head>
     <body>
+       <!-- Google Tag Manager (noscript) -->
+       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5TBPZR5"
+       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <?php include "header-nav.php" ?>
         <?php include "front_content.php" ?>
     </body>
--- time_table.php
+++ time_table.php
@@ -7,8 +7,19 @@
   <!-- <link rel="stylesheet" href="aaa.css" type="text/css"> -->
   <?php include "links.html" ?>
   <!-- <link rel="stylesheet" href="../stylesheet/time_table.css" type="text/css"> -->
+   <!-- Google Tag Manager -->
+ <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+ 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+ })(window,document,'script','dataLayer','GTM-T5TBPZR5');</script>
+ <!-- End Google Tag Manager -->
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+ <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5TBPZR5"
+ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+ <!-- End Google Tag Manager (noscript) -->
     <?php include "header-nav.php" ?>
     <div class="wrap">
     <div id="hontai">
