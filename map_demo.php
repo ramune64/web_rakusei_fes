80395ad428ffb4193f3a62866ed719d731e3d357
tqck <harata9mi0902@gmail.com>
2024-09-12 04:20:26 +0900

    a

--- map_demo.php
+++ map_demo.php
@@ -9,20 +9,11 @@
     $path = dirname(__FILE__);$path = str_replace("\\news","",$path);
     include($path."/links.html");
   ?>
-  <!-- Google Tag Manager -->
- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
- new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
- j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
- 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
- })(window,document,'script','dataLayer','GTM-T5TBPZR5');</script>
- <!-- End Google Tag Manager -->
+  
   <link rel="stylesheet" href="../stylesheet/map_demo.css" type="text/css">
 </head>
 <body>
-  <!-- Google Tag Manager (noscript) -->
- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5TBPZR5"
- height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
- <!-- End Google Tag Manager (noscript) -->
+  
   <div class="gray_back_map" id="gray_back_map"></div>
   <div class="over_view_box" id="ovh1a">
     <h1 class="over_view_map">Aクラス</h1>
