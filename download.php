9266dd89e433477cc8c59ec748aa29547abdbf6b
tqck <harata9mi0902@gmail.com>
2024-09-12 04:53:39 +0900

    aa

--- download.php
+++ download.php
@@ -5,20 +5,11 @@
 
     <?php include "links.html" ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T5TBPZR5');</script>
- <!-- End Google Tag Manager -->
+   
     <meta charset="UTF-8">
 </head>
 <body>
-    <!-- Google Tag Manager (noscript) -->
- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5TBPZR5"
- height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
- <!-- End Google Tag Manager (noscript) -->
+   
         <?php include "header-nav.php" ?>
         <div class="wrap">
         <div id="hontai">
@@ -34,7 +25,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         </div>
                         <div class="Download_intro item">
                             <h3>説明</h3>
-                            <p>第73回洛星文化祭～宴～<br>のパンフレットです！<br>プログラムパートが一生懸命作った集大成なので、文化祭に来る前や来校された際などにお使いください！！<br><u>9/6本邦初公開予定</u><!-- <br>試し読みは<a href="pamphlet.php">こちら</a> --></p>
+                            <p>第73回洛星文化祭～宴～<br>のパンフレットです！<br>プログラムパートが一生懸命作った集大成なので、文化祭に来る前や来校された際などにお使いください！！<br><!-- <br>試し読みは<a href="pamphlet.php">こちら</a> --></p>
                             <a href="pdfs/" class="Download_button" download><img src="image/download_red.png" alt="ダウンロードアイコン"><p>ダウンロード(PDF)</p></a>
                         </div>
                     </div>
