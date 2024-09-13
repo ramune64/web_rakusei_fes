7eb41b230b2233061491b33b82627579d3a7ab07
tqck <harata9mi0902@gmail.com>
2024-09-14 08:44:10 +0900

    nya

---
+++ 2news.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="UTF-8">
+  <title>第73回洛星文化祭～宴～</title>
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T5TBPZR5');</script>
+<!-- End Google Tag Manager -->
+  <?php include "links.html" ?>
+</head>
+<body>
+  <!-- Google Tag Manager (noscript) -->
+ <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5TBPZR5"
+ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+ <!-- End Google Tag Manager (noscript) -->
+<?php include "header-nav.php" ?>
+  <div class="wrap">
+       <div id="hontai">
+          <div class="main_content">
+             <div class="inner_content inner_top news" id="news0">
+                  <h1>2024/09/14<br>【重要】模擬店について</h1>
+                  <p>今年度の模擬店は昨年度と大きく異なるので詳細を確認してきてください。<br>販売場所、販売時間などの情報が載っています。
+                  <br><a href="shop.php">詳細はこちらから</a></p>
+             </div>
+          </div>
+       </div>
+     <?php include "footer.html" ?>
+ </div>
+</body>
+</html>
